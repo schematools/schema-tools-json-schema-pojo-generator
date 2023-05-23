4c430bb165e0d27e192929e33a3632a0193abc24
Stephen Nimmo <stephennimmo@gmail.com>
2023-05-23 09:04:41 -0500

    more advanced ref examples work

--- src/test/java/io/schematools/generator/JsonSchemaPojoGeneratorTest.java
+++ src/test/java/io/schematools/generator/JsonSchemaPojoGeneratorTest.java
@@ -10,4 +10,10 @@ public class JsonSchemaPojoGeneratorTest {
         jsonSchemaPojoGenerator.generate("src/test/resources/schema/basic", "target/generated-sources");
     }
 
+    @Test
+    void refexamples() {
+        JsonSchemaPojoGenerator jsonSchemaPojoGenerator = new JsonSchemaPojoGenerator();
+        jsonSchemaPojoGenerator.generate("src/test/resources/schema/refexamples", "target/generated-sources");
+    }
+
 }
